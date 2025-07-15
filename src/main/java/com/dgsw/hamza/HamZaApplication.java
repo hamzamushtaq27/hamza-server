09edaf351c553abc246ba06d925edd9582222672
lyhtll <a62346986@gmail.com>
2025-07-15 17:01:28 +0900

    INIT

---
+++ src/main/java/com/dgsw/hamza/HamZaApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.hamza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamZaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamZaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dgsw/hamza/HamZaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.hamza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamZaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
